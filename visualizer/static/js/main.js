5233b07decfee66815030547a385f2cbcf6fa767
Benjamin Riviere <benjamin.riviere@etu.utc.fr>
2016-05-10 17:55:22 +0200

    Cleaning some code.

--- visualizer/static/js/main.js
+++ visualizer/static/js/main.js
@@ -45,44 +45,42 @@ function generateStars(grade){
 	return stars;
 }
 
+// Generate template with UV information
 function uvMessage (selectedNode, uvwebData) {
 	uvwebData.desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacinia augue sed tellus luctus, a cursus enim suscipit. Phasellus laoreet blandit arcu fringilla tincidunt. Proin sit amet euismod magna. Cras luctus at arcu sed vehicula. Fusce ut ex molestie, bibendum turpis eget, congue tortor. Nulla sed sapien et justo faucibus mattis. Vestibulum blandit justo ac nisi lacinia tristique. Pellentesque eros lorem, facilisis a risus eget, porttitor interdum tellus. Proin sagittis lacus eu tellus pulvinar mattis.';
 
-	// var message = '<ul class="collection">';
-			// message += '<li class="collection-item center-align"><h5>' + selectedNode.id + ' - ' + selectedNode.attributes.nomUV + '</h5></li>';
-		var message = '<li class="collection-item">';
-				message +=	'<div class="row">';
-					message +=	'<div class="col s6 center-align">';
-						message +=	'<b>Note sur <a href="https://assos.utc.fr/uvweb/uv/' + selectedNode.id + '" target="_blank">UVWeb</a></b><br>';
-						message +=	'<div>' + (uvwebData.note ? generateStars(parseFloat(uvwebData.note)) : 'Note indisponible') + '</div>';
+	var message = '<li class="collection-item">';
+			message +=	'<div class="row">';
+				message +=	'<div class="col s6 center-align">';
+					message +=	'<b>Note sur <a href="https://assos.utc.fr/uvweb/uv/' + selectedNode.id + '" target="_blank">UVWeb</a></b><br>';
+					message +=	'<div>' + (uvwebData.note ? generateStars(parseFloat(uvwebData.note)) : 'Note indisponible') + '</div>';
+					message +=	'</div>';
+						message +=	'<div class="col s6 center-align">';
+							message +=	'<b>Taux de réussite</b><br />';
+							message +=	'<div>00,00% </div>';
 						message +=	'</div>';
-							message +=	'<div class="col s6 center-align">';
-								message +=	'<b>Taux de réussite</b><br />';
-								message +=	'<div>00,00% </div>';
-							message +=	'</div>';
-						message +=	'</div>';
+					message +=	'</div>';
+			message +=	'</div>';
+		message +=	'</li>';
+		message += '<li class="collection-item text-justify">' + uvwebData.desc.replace(/^(.{500}[^\s]*).*/, "$1") + '... <a href="#">Lire la suite</a></li>';
+		message +=	'<li class="collection-item">';
+			message +=	'<div class="row">';
+				message +=	'<div class="col s3 center-align">';
+					message +=	'<i class="material-icons">class</i><br><div>' + selectedNode.attributes.Cat + '</div>';
 				message +=	'</div>';
-			message +=	'</li>';
-			message += '<li class="collection-item" style="text-align: justify;">' + uvwebData.desc.replace(/^(.{500}[^\s]*).*/, "$1") + '... <a href="#">Lire la suite</a></li>';
-			message +=	'<li class="collection-item">';
-				message +=	'<div class="row">';
-					message +=	'<div class="col s3 center-align">';
-						message +=	'<i class="material-icons">class</i><br><div>' + selectedNode.attributes.Cat + '</div>';
-					message +=	'</div>';
-					message +=	'<div class="col s3 center-align">';
-						message +=	'<i class="material-icons">school</i><br><div>' + selectedNode.attributes.nbCredits + ' ECTS</div>';
-					message +=	'</div>';
-					message += '<div class="col s3 center-align">';
-						message +=	'<i class="material-icons">group</i><br><div>Effectif</div>';
-					message +=	'</div>';
-						message +=	'<div class="col s3 center-align"><i class="material-icons">access_time</i><br><div>Semestre</div>';
-					message +=	'</div>';
+				message +=	'<div class="col s3 center-align">';
+					message +=	'<i class="material-icons">school</i><br><div>' + selectedNode.attributes.nbCredits + ' ECTS</div>';
 				message +=	'</div>';
-			message +=	'</li>';
-			message +=	'<li class="collection-item center-align">';
-				message += '<button class="btn">Ajouter à mon parcours</button>';
-			message +=	'</li>';
-		// message += '</ul>';
+				message += '<div class="col s3 center-align">';
+					message +=	'<i class="material-icons">group</i><br><div>Effectif</div>';
+				message +=	'</div>';
+					message +=	'<div class="col s3 center-align"><i class="material-icons">access_time</i><br><div>Semestre</div>';
+				message +=	'</div>';
+			message +=	'</div>';
+		message +=	'</li>';
+		message +=	'<li class="collection-item center-align">';
+			message += '<button class="btn">Ajouter à mon parcours</button>';
+		message +=	'</li>';
 
 	return message;
 }
@@ -90,11 +88,13 @@ function uvMessage (selectedNode, uvwebData) {
 function changeInfobox () {
 	var message;
 	var selectedNode = activeState.nodes()[activeState.nodes().length - 1];
+
 	if (selectedNode.attributes.Type === 'UV'){
-		$('#right-menu-infoUV ul.collection').empty();
 		// Setting UV name and a loader
+		$('#right-menu-infoUV ul.collection').empty();
 		$('#right-menu-infoUV ul.collection').append('<li class="collection-item center-align"><h5>' + selectedNode.id + ' - ' + selectedNode.attributes.nomUV + '</h5></li>');
 		$('#right-menu-infoUV ul.collection').append('<li class="collection" id="uv-progress"><div class="progress"><div class="indeterminate"></div></div></li>');
+		
 		$.ajax('uvweb?uv=' + selectedNode.id, {
 			success: function (uvwebData) {
 				$('#uv-progress').remove();
@@ -122,10 +122,6 @@ function nodeInit (n) {
 	n.originalColor = n.color;
 	n.originalLabel = n.label;
 	n.disabled = false;
-	// n.data = {};
-	// n.data.properties = {};
-	// n.data.properties.categorie = n.attributes.Type;
-	// n.data.properties.couleur = n.originalColor;
 
 	// Change the shape depending on the node type (UV or Semestre)
 	if(n.attributes.Type === 'Semestre'){
